93e7d19a473df221d898dcc83bff05a1b2dd38e2
GilAxelGutierrezLara <93830551+GilAxelGutierrezLara@users.noreply.github.com>
2021-11-10 23:58:15 -0600

    Update Auto.java
    Agregue metodos Set y toString

--- Gutierrez-Mancera-RUNRUN/Auto.java
+++ Gutierrez-Mancera-RUNRUN/Auto.java
@@ -10,13 +10,15 @@ public class Auto{
   private String transmision;
   private String equipacion;
   private String nombre;
+  private double precio;
   //Constructor con args
-  public Auto(String nombre,String color,String modelo, String transmision, String equipacion){
+  public Auto(String nombre,String color,String modelo, String transmision, String equipacion,double precio){
     this.nombre=nombre;
     this.color=color;
     this.modelo=modelo;
     this.transmision=transmision;
     this.equipacion=equipacion;
+    this.precio=precio;
   }
   //Metodos Guetters
   /**
@@ -53,4 +55,37 @@ public class Auto{
   public String getEquipacion(){
     return this.equipacion;
   }
+  /**
+  *Metodo que devuelve el precio del auto
+  *@return El precio del auto
+  **/
+  public double getPrecio(){
+    return this.precio;
+  }
+  
+  //Metdodos set
+  public void setPrecio(double precio){
+    this.precio=precio;
+  }
+  public void setNombre(String nombre){
+    this.nombre=nombre;
+  }
+  public void setColor(String color){
+    this.color=color;
+  }
+  public void setModelo(String modelo){
+    this.modelo=modelo;
+  }
+  public void setTransmision(String transmision){
+    this.transmision=transmision;
+  }
+  public void setEquipacion(String equipacion){
+    this.equipacion=equipacion;
+  }
+  
+  
+  //Metodo toStriing
+  
+  public String toString(){
+    return "El auto "+nombre+", es de color "+color+", modelo "+modelo+", transmision tipo "+transmision+", equipacion "+equipacion+", y tiene un precio de "+precio+" pesos."
 }
