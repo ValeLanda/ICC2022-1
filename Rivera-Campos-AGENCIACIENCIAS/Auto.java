4ab18bf49fc9bf8f0b71a12b8b35966136163159
Jesús A. Rivera <jesalbertorm@ciencias.unam.mx>
2021-11-11 19:47:01 -0600

    Se agrega el Proyecto

---
+++ Rivera-Campos-AGENCIACIENCIAS/Auto.java
@@ -0,0 +1,21 @@
+/*
+ * Clase Auto 
+ * @author Jesus Rivera
+ * @author Rodrigo Campos
+ * @version 1.0
+ * */
+publi class Auto{
+    private String marca;
+    private String modelo;
+    private String color;
+    private int año;
+    private float precio;
+
+    public Auto(String marca, String modelo, String color, int año, float precio){
+	this.marca = marca;
+	this.modelo = modelo;
+	this.color = color;
+	this.año = año;
+	this.precio = precio;
+    }
+}
