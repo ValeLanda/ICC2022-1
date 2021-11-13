25e6f133d368e483bc61fabb837c3121045e733f
GilAxelGutierrezLara <93830551+GilAxelGutierrezLara@users.noreply.github.com>
2021-11-12 22:10:46 -0600

    Create AgenciaRUNRUN.java

---
+++ Gutierrez-Mancera-RUNRUN/AgenciaRUNRUN.java
@@ -0,0 +1,11 @@
+/**
+*@author Gil Axel GUtierrez Lara
+*@version 1.0
+**/
+public class Agencia{
+  public static void main(String args[]);{
+    Auto auto1=new Auto(Run1,rojo,2021,automatica,electrico,100000);
+      Sistem.out.println(auto1.toString());
+      
+  }
+}
