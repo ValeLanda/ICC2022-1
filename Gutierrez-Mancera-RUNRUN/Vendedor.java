6da4f1f0e3c6a9631370b4c109b6fbceae0b2abb
GilAxelGutierrezLara <93830551+GilAxelGutierrezLara@users.noreply.github.com>
2021-11-15 15:15:54 -0600

    Update Vendedor.java
    Agregue metodos vendedor

--- Gutierrez-Mancera-RUNRUN/Vendedor.java
+++ Gutierrez-Mancera-RUNRUN/Vendedor.java
@@ -3,11 +3,35 @@
 *@author Gil Axel Gutierrez Lara
 *@version 1.0
 **/
-publilc class Vendedor{
+public class Vendedor{
 
   //Atributos
-  private int autosEnVenta;
+  private String nombreV;
+  private int autosDisponibles;
   private int autosVendidos;
   
   
+  public Vendedor(String nombreV,int autosDisponibles,int autosVendidos){
+    this.nombreV=nombreV;
+    this.autosDisponibles=autosDisponibles;
+    this.autosVendidos=autosVendidos;
+  }  
+  public String getNombreV(){
+    return this.nombreV;
+  }
+  public int getAutosDisponibles(){
+    return this.autosDisponibles;
+  }
+  public int getAutosVendidos(){
+    return this.autosVendidos;  
+  }
+  public void setNombreV(String nombreV){
+    this.nombreV=nombreV;  
+  }
+  public void setAutosDisponibles(int autosDisponibles){
+    this.autosDisponibles=autosDisponibles;
+  }
+  public void setAutosVendidos(int autosVendidos){
+    this.autosVendidos=autosVendidos;
+  }
 }
