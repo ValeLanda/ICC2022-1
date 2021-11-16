9f18b9cb93025f4c9d2a116703b14b2e586946ba
JuanLuisRivera <juanluisrivera@ciencias.unam.mx>
2021-11-16 15:04:34 -0600

    Se añade la clase Agencia y se mejora la documentacion de la clase Auto

---
+++ Rivera-Peña-AgenciaX/Agencia.java
@@ -0,0 +1,270 @@
+/**
+*@author Juan Luis Rivera Ibarra
+*@version 1.0
+*/
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+*Clase Agencia
+*Programa que simula la operacion y administracion de una agencia de autos
+*@since 1.0
+*/
+public class Agencia{
+  /**
+  *Metodo main, el cual es forzoso y lleva  acabo la ejecucion del programa
+  */
+  public static void main(String[] args){
+    //Crea el arreglo de objetos de la clase Auto
+    ArrayList<Auto> autos = new ArrayList<>();
+
+    //Crea el arreglo de objetos de la clase Vendedor
+    //ArrayList<Vendedor> vendedores = new ArrayList<>();
+
+    //Crea el arreglo de objetos de la clase Comprador
+    //ArrayList<Comprador> compradores = new ArrayList<>();
+
+    //Se crea
+    Scanner input = new Scanner(System.in);
+
+    int menu0 = -1;
+
+    System.out.println("Escoge una opcion numerica del 0 al 4 para elegir la lista sobre la cual añadir/modificar/eliminar/buscar informacion: \n");
+    System.out.println("Compradores (1): \n");
+    System.out.println("Vendedores (2): \n");
+    System.out.println("Automoviles (3): \n");
+    System.out.println("Terminar programa (4): \n");
+    System.out.println("Volver a mostrar este menu (0): \n");
+
+    do {
+      if (menu0 != -1) {
+        System.out.println("\nEscoge una opcion numerica del 0 al 4 para elegir la lista sobre la cual añadir/modificar/eliminar/buscar/imprimir informacion: \n");
+      }
+      menu0 = input.nextInt();
+
+      //Se selecciono trabajar en la lista de Compradores
+      if (menu0 == 1){
+
+        System.out.println("\nEscoge una opcion numerica del 0 al 6 para realizar alguna operacion sobre la lista de Compradores: \n");
+        System.out.println("Añadir (1): \n");
+        System.out.println("Modificar (2): \n");
+        System.out.println("Eliminar (3): \n");
+        System.out.println("Buscar (4): \n");
+        System.out.println("Imprimir lista (5): \n");
+        System.out.println("Regresar al menu principal (6): \n");
+        System.out.println("Volver a mostrar este menu (0): \n");
+
+        int op = -1;
+
+        do {
+          if (op != -1) {
+            System.out.println("\nEscoge una opcion numerica del 0 al 6 para realizar alguna operacion sobre la lista de Compradores: \n");
+          }
+
+          op = input.nextInt();
+
+          //Añadir
+          if (op == 1) {
+            /*System.out.println(": \n");
+            compradores.add(new )*/
+          }
+
+          //Modificar
+          if (op == 2) {
+
+          }
+
+          //Eliminar
+          if (op == 3) {
+
+          }
+
+          //Buscar
+          if (op == 4) {
+
+          }
+
+          //Imprimir
+          /*if (op == 5) {
+            for (Auto comprador: compradores) {
+                System.out.println(comprador);
+                System.out.println("\n");
+            }
+          }*/
+
+          //Menu
+          if (op == 0) {
+            System.out.println("Escoge una opcion numerica del 0 al 5 para realizar alguna operacion sobre la lista de Compradores: \n");
+            System.out.println("Añadir (1): \n");
+            System.out.println("Modificar (2): \n");
+            System.out.println("Eliminar (3): \n");
+            System.out.println("Buscar (4): \n");
+            System.out.println("Regresar al menu principal (5): \n");
+            System.out.println("Volver a mostrar este menu (0): \n");
+          }
+
+        } while (op != 6);
+
+      }
+
+      //Se selecciono trabajar en la lista de Vendedores
+      if (menu0 == 2) {
+
+        System.out.println("\nEscoge una opcion numerica del 0 al 6 para realizar alguna operacion sobre la lista de Vendedores: \n");
+        System.out.println("Añadir (1): \n");
+        System.out.println("Modificar (2): \n");
+        System.out.println("Eliminar (3): \n");
+        System.out.println("Buscar (4): \n");
+        System.out.println("Imprimir lista (5): \n");
+        System.out.println("Regresar al menu principal (6): \n");
+        System.out.println("Volver a mostrar este menu (0): \n");
+
+        int op = -1;
+
+        do {
+          if (op != -1) {
+            System.out.println("\nEscoge una opcion numerica del 0 al 6 para realizar alguna operacion sobre la lista de Vendedores: \n");
+          }
+
+          op = input.nextInt();
+
+          //Añadir
+          if (op == 1) {
+
+          }
+
+          //Modificar
+          if (op == 2) {
+
+          }
+
+          //Eliminar
+          if (op == 3) {
+
+          }
+
+          //Buscar
+          if (op == 4) {
+
+          }
+
+          //Imprimir
+          if (op == 5) {
+            /*for (Auto vendedor: vendedores) {
+                System.out.println(vendedor);
+                System.out.println("\n");
+            }*/
+          }
+
+          //Menu
+          if (op == 0) {
+            System.out.println("Escoge una opcion numerica del 0 al 4 para realizar alguna operacion sobre la lista de Vendedores: \n");
+            System.out.println("Añadir (1): \n");
+            System.out.println("Modificar (2): \n");
+            System.out.println("Eliminar (3): \n");
+            System.out.println("Buscar (4): \n");
+            System.out.println("Regresar al menu principal (5): \n");
+            System.out.println("Volver a mostrar este menu (0): \n");
+          }
+
+        } while (op != 6);
+
+      }
+
+      //Se selecciono trabajar en la lista de Automoviles
+      if (menu0 == 3) {
+
+        System.out.println("\nEscoge una opcion numerica del 0 al 6 para realizar alguna operacion sobre la lista de Automoviles: \n");
+        System.out.println("Añadir (1): \n");
+        System.out.println("Modificar (2): \n");
+        System.out.println("Eliminar (3): \n");
+        System.out.println("Buscar (4): \n");
+        System.out.println("Imprimir lista (5): \n");
+        System.out.println("Regresar al menu principal (6): \n");
+        System.out.println("Volver a mostrar este menu (0): \n");
+
+        int op = -1;
+
+        do {
+          if (op != -1) {
+            System.out.println("\nEscoge una opcion numerica del 0 al 6 para realizar alguna operacion sobre la lista de Automoviles: \n");
+          }
+
+          op = input.nextInt();
+          //Limpia el buffer (necesario hacer despues de cambiar el tipo de dato que recibe el scanner)
+          input.nextLine();
+
+          //Añadir
+          if (op == 1) {
+            System.out.println("\nIngresa la marca del automovil: \n");
+            String marca = input.nextLine();
+            System.out.println("\nIngresa el modelo del automovil: \n");
+            String modelo = input.nextLine();
+            System.out.println("\nIngresa el año del automovil: \n");
+            int anio = input.nextInt();
+            input.nextLine();
+            System.out.println("\nIngresa el color del automovil: \n");
+            String color = input.nextLine();
+
+            //Crea y agrega el nuevo objeto auto a la lista
+            autos.add(new Auto(marca, modelo, anio, color));
+          }
+
+          //Modificar
+          if (op == 2) {
+
+          }
+
+          //Eliminar
+          if (op == 3) {
+
+          }
+
+          //Buscar
+          if (op == 4) {
+
+          }
+
+          //Imprimir
+          if (op == 5) {
+            for (Auto auto: autos) {
+                System.out.println(auto);
+                System.out.println("\n");
+            }
+          }
+
+          //Menu
+          if (op == 0) {
+            System.out.println("Escoge una opcion numerica del 0 al 5 para realizar alguna operacion sobre la lista de Automoviles: \n");
+            System.out.println("Añadir (1): \n");
+            System.out.println("Modificar (2): \n");
+            System.out.println("Eliminar (3): \n");
+            System.out.println("Buscar (4): \n");
+            System.out.println("Regresar al menu principal (5): \n");
+            System.out.println("Volver a mostrar este menu (0): \n");
+          }
+
+        } while (op != 6);
+
+      }
+
+      //Se selecciona reimprimir el menu principal
+      if (menu0 == 0) {
+        System.out.println("Escoge una opcion numerica del 0 al 4 para elegir la lista sobre la cual añadir/modificar/eliminar informacion: \n");
+        System.out.println("Compradores (1): \n");
+        System.out.println("vendedores (2): \n");
+        System.out.println("Automoviles (3): \n");
+        System.out.println("Terminar programa (4): \n");
+        System.out.println("Volver a mostrar el menu (0): \n");
+      }
+
+      //Se ingreso un valor de menu invalido
+      if (menu0 > 4 || menu0 < 0) {
+        System.out.println("La opcion ingresada es invalida, ingrese un valor numerico valido/n");
+      }
+
+    } while (menu0 != 4);
+
+    input.close();
+  }
+}
--- Rivera-Peña-AgenciaX/Auto.java
+++ Rivera-Peña-AgenciaX/Auto.java
@@ -1,6 +1,6 @@
 /**
 *@author Juan Luis Rivera Ibarra
-*@Version 1.1
+*@Version 1.2
 */
 /**
 *Clase Auto
@@ -9,7 +9,7 @@
 public class Auto {
     private String marca;
     private String modelo;
-    private int año;
+    private int anio;
     private String color;
     /**Metodo constructor que genera un automovil con los datos proporcionados por el usuario
     *@since 1.0
@@ -18,10 +18,42 @@ public class Auto {
     *@param año               Tipo de dato int que especifica el año en que se produjo el automovil
     *@param color             Tipo de datos String que especifica el color del automovil
     */
-    public Auto(String marca, String modelo, int año, String color){
+    public Auto(String marca, String modelo, int anio, String color){
       this.marca = marca;
       this.modelo = modelo;
-      this.año = año;
+      this.anio = anio;
+      this.color = color;
+    }
+
+    /**Cambia la marca del automovil por la marca en tipo de dato String que recibe el metodo
+    *@since 1.2
+    *@param marca    Tipo de dato String por la cual se remplazara la marca del automovil
+    */
+    public void setMarca(String marca){
+      this.marca = marca;
+    }
+
+    /**Cambia el modelo del automovil por el modelo del tipo de dato String que recibe el metodo
+    *@since 1.2
+    *@param modelo    Tipo de dato String por el cual se remplazara el modelo del automovil
+    */
+    public void setModelo(String modelo){
+      this.modelo = modelo;
+    }
+
+    /**Cambia el año del automovil por la año en tipo de dato int que recibe el metodo
+    *@since 1.2
+    *@param int    Tipo de dato int por la cual se remplazara el año del automovil
+    */
+    public void setAnio(int anio){
+      this.anio = anio;
+    }
+
+    /**Cambia el color del automovil por el color del tipo de dato String que recibe el metodo
+    *@since 1.2
+    *@param color    Tipo de dato String por el cual se remplazara el color del automovil
+    */
+    public void setColor(String color){
       this.color = color;
     }
 
@@ -42,11 +74,11 @@ public class Auto {
     }
 
     /**Metodo que regresa año de inicio de produccion del automovil en un dato tipo int
-    *@since Version 1.1
+    *@since 1.1
     *@return Valor en tipo de dato int referente al año de inicio de produccion del automovil
     */
-    public int getAño(){
-      return año;
+    public int getAnio(){
+      return anio;
     }
 
     /**Metodo que regresa el color del automovil en tipo de dato String
@@ -62,6 +94,6 @@ public class Auto {
     *@return Tipo de dato String que resume la informacion clave del automovil
     */
     public String toString(){
-      return ("Automovil marca: " + getMarca() + "\nmodelo: " + getModelo() + "\nAño: " + getAño()) + "\ncolor: " + getColor() + ".";
+      return ("Automovil marca: " + getMarca() + "\nmodelo: " + getModelo() + "\nAño: " + getAnio()) + "\ncolor: " + getColor() + ".";
     }
 }
