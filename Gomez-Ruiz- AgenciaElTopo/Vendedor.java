f330c666c1248bce07899a7446fca895620a4059
MaxoRuiz <maxruizg@comunidad.unam.mx>
2021-11-17 20:12:37 -0600

    FEAT: se documentó la clase Auto y se inició la clase vendedor

---
+++ Gomez-Ruiz- AgenciaElTopo/Vendedor.java
@@ -0,0 +1,29 @@
+public class Vendedor{
+
+    //Atributos de la clase
+    private String nombre;
+    private int identificador;
+    private String contrasenia;
+    private int estrellas;
+
+    /////////////////////////////////////////////
+    
+    //Constructor
+    public Vendedor(){
+	nombre = "Juan Diego Armando Muros Torres Mart\u00ednez";
+	identificador = 4563;
+	constrasenia = "SimplmenteJuan";
+	estrellas = 5;
+    }
+
+    /////////////////////////////////////////////
+
+    //Getters
+    public String getNombre(){
+	return this.nombre;
+    }
+
+    public int getIdentificador()
+	
+	
+       
