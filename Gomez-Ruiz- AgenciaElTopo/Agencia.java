8d4a6e5f422a855be5de3b028de8a5df7eb24b59
MaxoRuiz <maxruizg@comunidad.unam.mx>
2021-11-17 19:07:53 -0600

    FEAT: se agregó la clase auto

--- Gomez-Ruiz- AgenciaElTopo/Agencia.java
+++ Gomez-Ruiz- AgenciaElTopo/Agencia.java
@@ -1,5 +1,6 @@
 public class Agencia{
     public static void main(String args[]){
+
 	
     }	
 }
--- Gomez-Ruiz- AgenciaElTopo/Auto.java
+++ Gomez-Ruiz- AgenciaElTopo/Auto.java
@@ -8,16 +8,80 @@ public class Auto{
     private int potencia;
     private double precio;
 
-    ///////////////////////////////////////
+    /////////////////////////////////////////
 
     //Constructores
-    public Auto(){
-	marca = "Nissan";
-	modelo = "Versa";
-	color = "Rojo Escarlata";
-	anio = 2021;
-	potencia = 118;
-	precio = 10000.00;
+    public Auto(String marca,String modelo,String color,int anio,int potencia,double precio){
+	this.marca = marca;
+	this.modelo = modelo;
+	this.color = color;
+	this.anio = anio;
+	this.potencia = potencia;
+	this.precio = precio;
     }
 
-    public Auto(
+    /////////////////////////////////////////
+
+    //Getters
+    public String getMarca(){
+	return this.marca;
+    }
+
+    public String getModelo(){
+	return this.modelo;
+    }
+
+    public String getColor(){
+	return this.color;
+    }
+
+    public int getAnio(){
+	return this.anio;
+    }
+
+    public int getPotencia(){
+	return this.potencia;
+    }
+
+    public double getPrecio(){
+	return this.precio;
+    }
+
+    /////////////////////////////////////////
+
+    //Setters
+    public void setMarca(String marca){
+	this.marca = marca;
+    }
+
+    public void setModelo(String modelo){
+	this.modelo = modelo;
+    }
+
+    public void setColor(String color){
+	this.color = color;
+    }
+
+    public void setAnio (int anio){
+	this.anio = anio;
+    }
+
+    public void setPotencia (int potencia){
+	this.potencia = potencia;
+    }
+
+    public void setPrecio (double precio){
+	this.precio = precio;
+    }
+
+    ////////////////////////////////////////
+
+    //Método toString
+    public String toString(){
+	String s = "";
+	s += "\n Auto y sus caracter\u00edsiticas: \n-Marca: "+this.marca+"\n-Modelo: "+this.modelo+"\n-Color: "+this.color+"\n-A\u00f1o: "
+	    +this.anio+"\n-Potencia: "+this.potencia+" caballos de potencia \n-Precio: "+this.precio;
+	return s;
+    }
+	      
+    }
